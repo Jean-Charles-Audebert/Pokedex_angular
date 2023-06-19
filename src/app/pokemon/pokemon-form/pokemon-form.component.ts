95d22c61f14f1f096f5e05461e366e47d38248ab
Jean-Charles-Audebert <jc1932@gmail.com>
2023-06-19 19:25:03 +0200

    adding persistence

--- src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -33,11 +33,6 @@ export class PokemonFormComponent implements OnInit {
     }
   }
 
-  onSubmit() {
-    console.log("Submit form !");
-    this.router.navigate(["/pokemon", this.pokemon.id]);
-  }
-
   isTypesValid(type: string) : boolean {
     if(this.pokemon.types.length == 1 && this.hasType(type)){
       return false;
@@ -47,4 +42,16 @@ export class PokemonFormComponent implements OnInit {
     }
     return true;
   }
+
+  onSubmit() {
+   /*  if(this.isAddForm) {
+      this.pokemonService.addPokemon(this.pokemon)
+        .subscribe((pokemon: Pokemon) => this.router.navigate(['/pokemon', pokemon.id]));
+    } else { */
+      this.pokemonService.updatePokemon(this.pokemon)
+        .subscribe(() => this.router.navigate(['/pokemon', this.pokemon.id]));
+    //}
+  }
+
+ 
 }
--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,16 +1,79 @@
 import { Injectable } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
 import { Pokemon } from './pokemon';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable, catchError, of, tap } from 'rxjs';
 
 @Injectable()
 export class PokemonService {
-  getPokemonList(): Pokemon[] {
-    return POKEMONS;
+
+  constructor(private http: HttpClient) {}
+  
+/*   getPokemonList(): Observable<Pokemon[]> {
+    return this.http.get<Pokemon[]>('api/pokemons').pipe(
+      tap((pokemonList)=> console.table(pokemonList)),
+      catchError((error) => {
+        console.log(error);
+        return of([]);
+      })
+    );
   }
 
-  getPokemonById(pokemonId: number): Pokemon|undefined {
-    return POKEMONS.find(pokemon => pokemon.id == pokemonId);
+  getPokemonById(pokemonId: number): Observable<Pokemon|undefined> {
+    return this.http.get<Pokemon>('api/pokemons/${pokemonId}').pipe(
+      tap((pokemon)=> console.table(pokemon)),
+      catchError((error) => {
+        console.log(error);
+        return of(undefined);
+      })
+    )
+  } */
+
+  //FACTO
+
+  getPokemonList(): Observable<Pokemon[]> {
+    return this.http.get<Pokemon[]>('api/pokemons').pipe(
+      tap((response)=> this.log(response)),
+      catchError((error)=> this.handleError(error, []))
+    );
   }
+
+  getPokemonById(pokemonId: number): Observable<Pokemon|undefined> {
+    return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
+      tap((response)=> this.log(response)),
+      catchError((error)=> this.handleError(error, []))
+    );
+  }
+
+  //PERSISTENCE
+
+  updatePokemon(pokemon: Pokemon): Observable<null> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+
+    return this.http.put('api/pokemons', pokemon, httpOptions).pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, null))
+    );
+  }
+
+  deletePokemonById(pokemonId: number): Observable<null> {
+    return this.http.delete(`api/pokemons/${pokemonId}`).pipe(
+      tap((response)=> this.log(response)),
+      catchError((error)=> this.handleError(error, []))
+    );
+  } 
+
+  private log(response: any) {
+    console.table(response);
+  }
+
+  private handleError(error: Error, errorValue: any) {
+    console.error(error);
+    return of(errorValue);
+  }
+
+
 
   getPokemonTypeList(): string[] {
     return [
